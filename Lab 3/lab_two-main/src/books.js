74bab0a7a181e20d42be51a4e5618499d898227d
JackTalbot <G00395652@atu.ie>
2023-10-13 10:46:48 +0100

    lab3

---
+++ Lab 3/lab_two-main/src/books.js
@@ -0,0 +1,8 @@
+function Books(props){
+
+    return
+    <div>
+        <h2>Hello from the Books component</h2>
+        
+    </div>
+}
---
+++ Lab 3/lab_two-main/src/components/bookItem.js
@@ -0,0 +1,11 @@
+import { Component } from "react"
+
+function BookItem(){
+    return(
+        <h2>
+            Hello From BookItem Component
+        </h2>
+    )
+}
+
+export default BookItem;
