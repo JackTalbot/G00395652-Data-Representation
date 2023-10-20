c35a4758a78f8e3e1014b606f8a0aaa3cb0d17ac
JackTalbot <G00395652@atu.ie>
2023-10-20 10:36:23 +0100

    Lab 4 work, needs comments

--- lab_three-main/src/components/create.js
+++ lab_three-main/src/components/create.js
@@ -1,9 +1,57 @@
-function Create(){
+import React, { useState } from "react";
+
+function Create() {
+
+    const [title, setTitle] = useState("");
+    const [cover, setCover] = useState("");
+    const [author, setAuthor] = useState("");
+
+    const handleSubmit = (e) => {
+        // prevent the default behavior of the form submit
+        e.preventDefault();
+        console.log(title, cover, author);
+    }
 
     return(
         <div>
-            <h2>Hello from create Component!</h2>
+            <form onSubmit={handleSubmit}>
+            <div className="form-group">
+                <label> Book Title: </label>
+                <input type="text"
+                className="form-control"
+                value={title}
+                onChange={(e) => { setTitle(e.target.value) }}
+            />
+            {/* Takes Book Title from UI */}
+            </div>
+
+            <div className="form-group">
+                <label> Book Cover: </label>
+                <input type="text"
+                className="form-control"
+                value={cover}
+                onChange={(e) => { setCover(e.target.value) }}
+                />
+            {/* Takes Book Cover from UI */}
+            </div>
+
+            <div className="form-group">
+                <label> Book Author: </label>
+                <input type="text"
+                className="form-control"
+                value={author}
+                onChange={(e) => { setAuthor(e.target.value) }}
+                />
+            {/* Takes Book Author from UI */}
+            </div>
+
+            <div> 
+                <input type="submit" value="Add Book" className="btn btn-primary" /> &nbsp;
+            {/* Adds Button for taking inputted information */}
+            </div>
+            </form>
         </div>
+
     );
 
 }
