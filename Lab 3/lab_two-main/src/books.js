887e28480f928a0e80cce373ba00c479ad68bdf8
JackTalbot <G00395652@atu.ie>
2023-11-23 22:19:10 +0000

    Catching up to Lab 7

--- Lab 3/lab_two-main/src/books.js
+++
@@ -1,8 +0,0 @@
-function Books(props){
-
-    return
-    <div>
-        <h2>Hello from the Books component</h2>
-        
-    </div>
-}
--- Lab 3/lab_two-main/src/components/bookItem.js
+++ Lab 3/lab_two-main/src/components/bookItem.js
@@ -1,11 +1,26 @@
-import { Component } from "react"
+import Card from "react-bootstrap/Card";
 
 function BookItem(){
     return(
-        <h2>
-            Hello From BookItem Component
-        </h2>
-    )
+        <div>
+        <Card>
+            <Card.Header>{props.myBook.title}</Card.Header>
+            <Card.Body>
+                <blockquote className="blockquote mb-0">
+                    <img src={props.myBook.cover}></img>
+                    <footer>
+                        {props.myBook.author
+                        
+                        }
+                    </footer>
+                </blockquote>
+            </Card.Body>
+        </Card>
+        {/* <h3>{props.myBook.title}</h3>
+        <img src={props.myBook.thumbnailUrl}></img>
+        <p>{props.myBook.authors[0]}</p> */}
+    </div>
+    );
 }
 
 export default BookItem;
