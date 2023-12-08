69f9f6e35ee17a9bc1e5ab92d6b31246c17ea33d
JackTalbot <G00395652@atu.ie>
2023-12-08 09:53:39 +0000

    Verified Axios and fixing code

--- Lab 3/lab_two-main/src/components/books.js
+++ Lab 3/lab_two-main/src/components/books.js
@@ -3,7 +3,9 @@ import BookItem from './bookItem';
 function Books(props) {
     return props.mybooks.map(
         (book) =>{
-            return <BookItem mybook={book}  key={book._id} reload={()=>{props.reload}}></BookItem>
+            return <BookItem 
+            mybook={book}  key={book._id} reload={()=>{props.reload}}>
+            </BookItem>
         }
     );
 }
