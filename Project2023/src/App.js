6b9ad6404bb7cbd2c375c7cee5e30d82f0cf9533
JackTalbot <G00395652@atu.ie>
2023-12-18 10:41:48 +0000

    Project Begining

--- Project2023/src/App.js
+++ Project2023/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Content from './components/content';
+import Footer from './components/footer';
+import Header from './components/header';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -9,21 +11,21 @@ import Create from './components/create';
 import Read from './components/read';
 import Edit from './components/edit';
 
-function App() { {/* Main App Script */}
+function App() {
   return (
-    <BrowserRouter> {/* Browser Router Script */}
+    <BrowserRouter>
     <div className="App">
        <Navbar bg="dark" data-bs-theme="dark">
-        <Container> {/* Navbar Container */}
+        <Container>
           <Navbar.Brand href="/">Navbar</Navbar.Brand>
-          <Nav className="me-auto"> {/* Links to Web Pages */}
+          <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/create">Create</Nav.Link>
             <Nav.Link href="/read">Read</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      <Routes> {/* Routes for Links */}
+      <Routes>
         <Route path='/' element={<Content></Content>}></Route>
         <Route path='/read' element={<Read></Read>}></Route>
         <Route path='/create' element={<Create></Create>}></Route>
@@ -32,8 +34,6 @@ function App() { {/* Main App Script */}
       {/* <Header></Header>
       <Content></Content>
       <Footer /> */}
-     
-
     </div>
     </BrowserRouter>
   );
