98f12ab97f581fd68bf3fd620c614a76cf8dd9d1
JackTalbot <G00395652@atu.ie>
2023-12-21 17:30:08 +0000

    Restarting due to issues

---
+++ Project2023/src/components/dist/showItem.dev.js
@@ -0,0 +1,23 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _Card = _interopRequireDefault(require("react-bootstrap/Card"));
+
+var _reactRouterDom = require("react-router-dom");
+
+var _Button = _interopRequireDefault(require("react-bootstrap/Button"));
+
+var _axios = _interopRequireDefault(require("axios"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function ShowItem(props) {
+  return;
+}
+
+var _default = ShowItem;
+exports["default"] = _default;
--- Project2023/src/components/showItem.js
+++ Project2023/src/components/showItem.js
@@ -2,36 +2,27 @@ import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
+function ShowItem(props) {
 
-function AddShow(props) {
     return (
         <div>
             <Card>
-                <Card.Header>{props.myShow.title}</Card.Header>
+                <Card.Header>
+                    {props.myShow.title}
+                </Card.Header>
                 <Card.Body>
                     <blockquote className="blockquote mb-0">
-                        <img src={props.myShow.cover}></img>
+                        <img src={props.myShow.poster}></img>
                         <footer>
-                            {
-                            props.myShow.author
-                            }
+                            {props.myShow.year}
                         </footer>
                     </blockquote>
                 </Card.Body>
-                <Link to={'/edit/' + props.myShow._id} className='btn btn-primary'>Edit</Link>
-                <Button variant='danger' onClick={
-                    (e) => {
-                        e.preventDefault();
-
-                        axios.delete('http://localhost:4000/api/show/' + props.myShow._id)
-                            .then((res) => {
-                                let reload = props.Reload();
-                            })
-                            .catch();
-                    }
-                }>Delete</Button>
+                <Link to={'/edit/'+props.myShow._id} className='btn btn-primary'>Edit</Link>
             </Card>
         </div>
     )
+
 }
-export default AddShow;
+
+export default ShowItem;
